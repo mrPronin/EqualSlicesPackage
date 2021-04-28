872617b0b785592a1e006f93f29988037443c88d
Alexander Pronin <pronin.alx@gmail.com>
2021-04-28 22:53:34 +0200

    equalSlices now public

--- Sources/EqualSlicesPackage/EqualSlicesPackage.swift
+++ Sources/EqualSlicesPackage/EqualSlicesPackage.swift
@@ -1,9 +1,4 @@
-/*
-struct EqualSlicesPackage {
-    var text = "Hello, World!"
-}
-*/
-func equalSlices(totalSlices: Int, recipients: Int, perRecipientSlices slices: Int) -> Bool {
+public func equalSlices(totalSlices: Int, recipients: Int, perRecipientSlices slices: Int) -> Bool {
     guard recipients >= 0 else { return true }
     return totalSlices >= recipients * slices
 }
